0d887d679316651bc5de3a1ae9a94876c6d3b15c
Reuben-Ekow-Yawson <87475624+Reuben-Ekow-Yawson@users.noreply.github.com>
2022-02-22 21:28:59 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+"use strict";
+exports.__esModule = true;
+var restaurants_1 = require("./restaurants");
+var orders_1 = require("./orders");
+/// Add your getMaxPrice() function below:
+function getMaxPrice(price) {
+    switch (price) {
+        case orders_1.PriceBracket.low:
+            return 10.0;
+        case orders_1.PriceBracket.Medium:
+            return 20.0;
+        case orders_1.PriceBracket.High:
+            return 30.0;
+    }
+}
+/// Add your getOrders() function below:
+function getOrders(price, orders) {
+    var filteredOrders = [];
+    orders[0].filter(function (order) { return order.price <= getMaxPrice(price); });
+    orders.forEach(function (restaurant) {
+        var result = restaurant.filter(function (order) { return order.price <= getMaxPrice(price); });
+        filteredOrders.push(result);
+    });
+    return filteredOrders;
+}
+/// Add your printOrders() function below:
+function printOrders(restaurants, orders) {
+    restaurants.forEach(function (restaurant, index) {
+        if (orders[index].length > 0) {
+            console.log(restaurant.name);
+            orders[index].forEach(function (order) {
+                console.log(" - ".concat(order.name, ": ").concat(order.price));
+            });
+        }
+    });
+}
+/// Main
+var elligibleOrders = getOrders(orders_1.PriceBracket.low, orders_1.orders);
+//console.log(elligibleOrders);
+printOrders(restaurants_1.restaurants, elligibleOrders);
